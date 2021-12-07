29121261830eb306917444c5c74656a08fa10692
Gaston Caminiti <gastoncaminiti@outlook.com>
2021-12-07 19:51:59 -0300

    Commit 1 [NAVMESH]

--- AFTER - NAVMESH Y DICTIONARY SERIALIZATION/Assets/Scripts/PlayerController.cs
+++ AFTER - NAVMESH Y DICTIONARY SERIALIZATION/Assets/Scripts/PlayerController.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private GameObject pointer;
     // Start is called before the first frame update
+    private NavMeshAgent playerAgent;
+
     void Start()
     {
-        
+        playerAgent = GetComponent<NavMeshAgent>();
+        //playerAgent.updateRotation = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            pointer.transform.position = GetPositionTo(Input.mousePosition);
+        }
+        playerAgent.destination = pointer.transform.position;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -23,5 +32,16 @@ public class PlayerController : MonoBehaviour
             GameManager.addScore();
             Destroy(other.gameObject);
         }
+    }
+
+    private Vector3 GetPositionTo(Vector3 newPosition)
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(newPosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            return hit.point;
+        }
+        return Vector3.zero;
     }
 }
